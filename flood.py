0a331f8b959ae9431e4b5a6c52c0387b11c6471b
Ronnypetson <rsronnypetson4@gmail.com>
2017-09-11 23:07:48 -0300

    flood fixed

--- flood.py
+++ flood.py
@@ -2,7 +2,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-im_path = 'teste_.jpg' # '../TREM-HAND.tif'
+im_path = 'handwriting.png' # '../TREM-HAND.tif'
+blocks_fn = 'teste_'
 
 #
 #gim = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
@@ -13,6 +14,18 @@ im_path = 'teste_.jpg' # '../TREM-HAND.tif'
 #plt.imshow(im)
 #ptl.show()
 #
+
+def max_(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+
+def min_(a,b):
+    if a < b:
+        return a
+    else:
+        return b
 
 class Flood:
     def __init__(self,grid):
@@ -26,16 +39,24 @@ class Flood:
         for i in range(r):
             for j in range(c):
                 if not self.visited[i][j]:
-                    self.fill(i,j,self.grid[i][j])
-                    self.floods.append([i,j])
+                    k0,l0,k1,l1 = self.fill(i,j,self.grid[i][j])
+                    self.floods.append([k0,l0,k1,l1])
     
     def fill(self,i,j,t):
         r,c = self.grid.shape
         s = [[i,j]]
+        kmax = 0
+        lmax = 0
+        kmin = r
+        lmin = c
         while len(s) > 0:
             u = s.pop()
             k = u[0]
             l = u[1]
+            kmax = max_(kmax,k)
+            lmax = max_(lmax,l)
+            kmin = min_(kmin,k)
+            lmin = min_(lmin,l)
             if k < 0 or l < 0 or k >= r or l >= c or self.visited[k][l]:
                 continue
             if self.grid[k][l] == t:
@@ -44,12 +65,19 @@ class Flood:
                 s.append([k,l+1])
                 s.append([k-1,l])
                 s.append([k,l-1])
+        return [kmin,lmin,kmax,lmax]
 
 im = cv2.imread(im_path,0)
-im = cv2.medianBlur(im,5)
-th = cv2.adaptiveThreshold(im,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY,11,2)
+#im = cv2.medianBlur(im,5)
+#th = cv2.adaptiveThreshold(im,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+th = cv2.adaptiveThreshold(im,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
 f = Flood(th)
 f.flood_fill()
-print(len(f.floods))
+for i in range(len(f.floods)):
+    ui = f.floods[i][0]
+    uj = f.floods[i][1]
+    li = f.floods[i][2]
+    lj = f.floods[i][3]
+    #print(ui,uj,li,lj)
+    cv2.imwrite(blocks_fn+str(i)+'.jpg',im[ui:li+1,uj:lj+1])
 
